ff52e81e42fd41f2f29ae0d66ffe040b8e8dba3c
Siva <jmsiva123@gmail.com>
2023-04-09 10:00:05 -0400

    Siva643/Laravel_ecommerce

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\ProductController;
 Route::get('/login', function () {
     return view('login');
 });
+
 Route::get("logout", function(){
      Session::forget('user');
     return redirect('/login');
